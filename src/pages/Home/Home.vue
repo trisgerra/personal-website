1751d0021a396f3c47309515ec27221ee3c79b15
Andrea Di Giacomo <andrea.digiacomo@live.it>
2019-07-13 00:10:53 +0200

    add experience and skills card

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,17 +1,43 @@
 <template>
   <div class="home">
     <head-hero></head-hero>
+    <div class="card-container">
+      <div class="columns">
+        <div v-for="card in cards" v-bind:key="card.cardName" class="column home-card">
+          <home-card :card="card"></home-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HeadHero from './HeadHero/HeadHero.vue';
+import HomeCard from './HomeCard/HomeCard.vue';
+import { homeCards, ICard } from './homeCardItems';
 
 @Component({
   components: {
     HeadHero,
+    HomeCard,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  cards: ICard[] = homeCards;
+}
 </script>
+
+<style lang="scss" scoped>
+.columns {
+    margin: 0px;
+    width: 100%;
+}
+.column {
+    padding:0px;
+}
+.card-container {
+  border-top: 3px solid #FF0000;
+  border-right: 3px solid #FF0000;
+}
+</style>
---
+++ src/pages/Home/HomeCard/__tests__/HomeCard.spec.ts
@@ -0,0 +1,30 @@
+import { Wrapper } from '@vue/test-utils';
+import HomeCard from '../HomeCard.vue';
+import createWrapper from '@/utils/CreateWrapper';
+import { homeCards } from '../../homeCardItems';
+
+describe('HomeCard', () => {
+  const wrapper: Wrapper<HomeCard> = createWrapper(HomeCard, {
+    propsData: {
+      card: homeCards[0],
+    },
+  });
+
+  it('should render as expected', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('wrapper is a card', () => {
+    expect(wrapper.is('.card')).toBeTruthy();
+  });
+
+  it('contains the correct title', () => {
+    expect(wrapper.findAll('.card-title').length).toBe(1);
+    expect(wrapper.find('.card-title').text()).toEqual('WORK EXPERIENCE');
+  });
+
+  it('contains the correct number of item', () => {
+    expect(wrapper.findAll('.item').length).toBe(8);
+    expect(wrapper.find('.item').text()).toEqual('2012 - iOS Internship');
+  });
+});
